de9c6440aefc6cee84b8dc4c9523775e3983d8c1
wores <ww.dev.publish@gmail.com>
2019-04-02 22:38:13 +0900

    update

--- greet/greet.go
+++ greet/greet.go
@@ -3,6 +3,12 @@ package greet
 import "fmt"
 
 func Greet(name string, lang string) string {
+	if name == lang {
+		fmt.Println("name == lang")
+	} else {
+		fmt.Println("name != lang")
+	}
+
 	fmt.Println("dododo")
 	switch lang {
 	case "en":
